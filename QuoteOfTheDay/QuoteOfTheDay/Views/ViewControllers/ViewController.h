37c73d2bc4994f0b69ca053ecbcb204cf55e19c6
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-11-18 10:05:00 -0700

    Initial commit

---
+++ QuoteOfTheDay/QuoteOfTheDay/Views/ViewControllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuoteOfTheDay
+//
+//  Created by RYAN GREENBURG on 11/18/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuoteOfTheDay/QuoteOfTheDay/Views/ViewControllers/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  QuoteOfTheDay
+//
+//  Created by RYAN GREENBURG on 11/18/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
